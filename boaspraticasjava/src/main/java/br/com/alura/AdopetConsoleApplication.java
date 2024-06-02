1dd5e9f798298e454840a9c1c10e194f005ca019
João Carlos Pontes Albuquerque Filho <carlosalbuquerque@mobitbrasil.com.br>
2024-06-02 17:18:51 -0300

    Adicao boas praticas java.

---
+++ boaspraticasjava/src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -0,0 +1,42 @@
+package br.com.alura;
+
+import java.util.Scanner;
+
+public class AdopetConsoleApplication {
+
+    public static void main(String[] args) {
+        CommandExecutor executor = new CommandExecutor();
+
+        System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
+        try {
+            int opcaoEscolhida = 0;
+            while (opcaoEscolhida != 5) {
+
+                exibeMenu();
+
+                String textoDigitado = new Scanner(System.in).nextLine();
+                opcaoEscolhida = Integer.parseInt(textoDigitado);
+
+                switch (opcaoEscolhida){
+                    case 1 -> executor.executeCommand(new ListarAbrigoCommand());
+                    case 2 -> executor.executeCommand(new CadastrarAbrigoCommand());
+                    case 3 -> executor.executeCommand(new ListarPetsCommand());
+                    case 4 -> executor.executeCommand(new ImportarPetsAbrigoCommand());
+                    case 5 -> System.exit(0);
+                    default -> System.out.println("NÚMERO INVÁLIDO!");
+                }
+            }
+            System.out.println("Finalizando o programa...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void exibeMenu() {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Listar abrigos cadastrados");
+        System.out.println("2 -> Cadastrar novo abrigo");
+        System.out.println("3 -> Listar pets do abrigo");
+        System.out.println("4 -> Importar pets do abrigo");
+        System.out.println("5 -> Sair");
+    }
+}
---
+++ boaspraticasjava/src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -0,0 +1,34 @@
+package br.com.alura.client;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClientHttpConfiguration {
+
+    public HttpResponse<String> fazGet(String url) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> fazPost(String url, Object object) throws IOException, InterruptedException {
+
+        System.out.println(new Gson().toJson(object));
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(new Gson().toJson(object)))
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
